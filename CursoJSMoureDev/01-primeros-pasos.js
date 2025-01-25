f8d09eaa810aa6a9a75a3b01bd2c594711d9374f
Juitomg <juitomg@gmail.com>
2025-01-25 23:41:49 +0100

    Primeros pasos

--- CursoJSMoureDev/01-primeros-pasos.js
+++ CursoJSMoureDev/01-primeros-pasos.js
@@ -1,2 +1,35 @@
+// 1. Escribe un comentario en una línea.
+
+// Comentario de una línea
+
+// 2. Escribe un comentario en varias líneas.
+
+/*
+Comentario
+Varias
+Líneas
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos.
+
+
+// 4. Imprime por consola el valor de todas las variables.
+
+// 5. Imprime por consola el tipo de todas las variables.
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo.
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo.
+
+// 8. Declara constantes con valores asociados a todos lostipos de datos primitivos.
+
+// 9. A continuación, modifica los valores de las constantes.
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse.
+
+
+
+
+
 console.log("Hola mundo")
 
