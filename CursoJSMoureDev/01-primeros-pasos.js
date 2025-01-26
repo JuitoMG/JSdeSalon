0aacf7a66468d09af39869e03b7e6aed38c15990
Juitomg <juitomg@gmail.com>
2025-01-26 16:53:41 +0100

    Primeros Pasos acabado

--- CursoJSMoureDev/01-primeros-pasos.js
+++ CursoJSMoureDev/01-primeros-pasos.js
@@ -13,24 +13,61 @@ Líneas
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos.
 
+let varString = "Hola"
+let varInt = 34
+let varFloat = 4.5
+let varBool = true
+let underfinedValue //Valor aun no definido todavía
+let varNull = null
+let varSymbol = Symbol("miSimbolo")
+let varBigInt = BigInt(2345673456327485687934652783456287346529738567)
+let varBigInt2 = 2345673456327485687934652783456287346529738567n
+
+
 
 // 4. Imprime por consola el valor de todas las variables.
 
+console.log(varString)
+console.log(varInt)
+console.log(varFloat)
+console.log(varBool)
+console.log(underfinedValue)
+console.log(varNull)
+console.log(varSymbol)
+console.log(varBigInt)
+console.log(varBigInt2)
+
+
 // 5. Imprime por consola el tipo de todas las variables.
+
+console.log(typeof varString)
+console.log(typeof varInt)
+console.log(typeof varFloat)
+console.log(typeof varBool)
+console.log(typeof underfinedValue)
+console.log(typeof varNull)
+console.log(typeof varSymbol)
+console.log(typeof varBigInt)
+console.log(typeof varBigInt2)
+
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo.
 
+varString = "Otra cosa"
+
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo.
+
+varString = 43
+console.log(varString)
 
 // 8. Declara constantes con valores asociados a todos lostipos de datos primitivos.
 
+const constanteString = "Hola"
+
 // 9. A continuación, modifica los valores de las constantes.
+//constanteString = "sdaf"
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse.
 
-
-
-
-
-console.log("Hola mundo")
+// Hecho.
 
