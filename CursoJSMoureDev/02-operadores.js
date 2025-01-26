1049ed76611c0d5689ec86abf4c26a6b9d11afe1
Juitomg <juitomg@gmail.com>
2025-01-26 17:01:31 +0100

    operadores

---
+++ CursoJSMoureDev/02-operadores.js
@@ -0,0 +1,33 @@
+// 1. Crea una variable para cada operación aritmética.
+
+let suma = 2+2
+let resta = 2-1
+let multiplicacion = 2*3
+let division = 2/3
+let resto = 10%3
+
+// 2. Crea una variable para cada tipo de operación deasignación, que haga uso de las variables utilizadaspara las operaciones aritméticas.
+
+
+// 3. Imprime 5 comparaciones verdades con diferentesoperadores de comparación.
+
+
+// 4. Imprime 5 comparaciones falsas con diferentes operadoresde comparación.
+
+
+// 5. Utiliza el operador lógico and.
+
+
+// 6. Utiliza el operador lógico or.
+
+
+// 7. Combina ambos operadores lógicos.
+
+
+// 8. Añade alguna negación.
+
+
+// 9. Utiliza el operador ternario.
+
+
+// 10. Combina operadores aritméticos, de comparáción ylógicas.
